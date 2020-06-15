fcb1ab9f28d16b57b691b04742cf31ffe056eb0b
Alex Butler <alexheretic@gmail.com>
2020-06-15 08:09:40 +0100

    Add font-rs copyright to raster.rs

--- rasterizer/src/raster.rs
+++ rasterizer/src/raster.rs
@@ -1,4 +1,20 @@
 // Forked/repurposed from `font-rs` code: https://github.com/raphlinus/font-rs
+// Copyright 2015 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Modifications copyright (C) 2020 Alex Butler
+//
 // Cubic bezier drawing adapted from stb_truetype: https://github.com/nothings/stb
 #[cfg(all(feature = "libm", not(feature = "std")))]
 use crate::nostd_float::FloatExt;
