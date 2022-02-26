7dd5be2a809e05faa61ee11c26ca41306ef20520
Alex Butler <alexheretic@gmail.com>
2022-02-26 08:31:04 +0000

    Always close outlines
    Fixes cantarell 'f' outlining

--- dev/tests/render_reference.rs
+++ dev/tests/render_reference.rs
@@ -6,6 +6,7 @@ use std::{env, io::Cursor, path::PathBuf};
 const OPENS_SANS_ITALIC: &[u8] = include_bytes!("../fonts/OpenSans-Italic.ttf");
 const DEJA_VU_MONO: &[u8] = include_bytes!("../fonts/DejaVuSansMono.ttf");
 const EXO2_TTF: &[u8] = include_bytes!("../fonts/Exo2-Light.ttf");
+const CANTARELL_VF: &[u8] = include_bytes!("../fonts/Cantarell-VF.otf");
 const EXO2_OTF: &[u8] = include_bytes!("../fonts/Exo2-Light.otf");
 
 macro_rules! compare_image {
@@ -36,18 +37,19 @@ macro_rules! compare_image {
 }
 
 /// Return target directory accounting for env var `CARGO_TARGET_DIR`.
-fn target_dir() -> PathBuf {
-    env::var("CARGO_TARGET_DIR")
+fn temp_path(name: impl AsRef<std::path::Path>) -> PathBuf {
+    let mut path = env::var("CARGO_TARGET_DIR")
         .map(PathBuf::from)
-        .unwrap_or_else(|_| PathBuf::from("../target"))
+        .unwrap_or_else(|_| PathBuf::from("../target"));
+
+    path.push(name);
+    path
 }
 
 #[test]
 fn reference_draw_ttf_w() {
     let new_image = draw_grey_image(dev::rasterize_ttf_w());
-    new_image
-        .save(target_dir().with_file_name("new_ttf_w.png"))
-        .unwrap();
+    new_image.save(temp_path("new_ttf_w.png")).unwrap();
     compare_image!(new_image, include_bytes!("reference_ttf_w.png"));
 }
 
@@ -55,18 +57,14 @@ fn reference_draw_ttf_w() {
 fn reference_outline_draw_ttf_w() {
     let font = FontRef::try_from_slice(DEJA_VU_MONO).unwrap();
     let new_image = outline_draw(font, 'w', 16.0);
-    new_image
-        .save(target_dir().with_file_name("new_outlined_ttf_w.png"))
-        .unwrap();
+    new_image.save(temp_path("new_outlined_ttf_w.png")).unwrap();
     compare_image!(new_image, include_bytes!("reference_ttf_w.png"));
 }
 
 #[test]
 fn reference_draw_ttf_iota() {
     let new_image = draw_grey_image(dev::rasterize_ttf_iota());
-    new_image
-        .save(target_dir().with_file_name("new_ttf_iota.png"))
-        .unwrap();
+    new_image.save(temp_path("new_ttf_iota.png")).unwrap();
     compare_image!(new_image, include_bytes!("reference_ttf_iota.png"));
 }
 
@@ -75,7 +73,7 @@ fn reference_outline_draw_ttf_iota() {
     let font = FontRef::try_from_slice(OPENS_SANS_ITALIC).unwrap();
     let new_image = outline_draw(font, 'ΐ', 60.0);
     new_image
-        .save(target_dir().with_file_name("new_outlined_ttf_iota.png"))
+        .save(temp_path("new_outlined_ttf_iota.png"))
         .unwrap();
     compare_image!(new_image, include_bytes!("reference_ttf_iota.png"));
 }
@@ -83,9 +81,7 @@ fn reference_outline_draw_ttf_iota() {
 #[test]
 fn reference_draw_ttf_biohazard() {
     let new_image = draw_grey_image(dev::rasterize_ttf_biohazard());
-    new_image
-        .save(target_dir().with_file_name("new_ttf_biohazard.png"))
-        .unwrap();
+    new_image.save(temp_path("new_ttf_biohazard.png")).unwrap();
     compare_image!(new_image, include_bytes!("reference_ttf_biohazard.png"));
 }
 
@@ -94,7 +90,7 @@ fn reference_outline_draw_ttf_biohazard() {
     let font = FontRef::try_from_slice(DEJA_VU_MONO).unwrap();
     let new_image = outline_draw(font, '\u{2623}', 600.0);
     new_image
-        .save(target_dir().with_file_name("new_outlined_ttf_biohazard.png"))
+        .save(temp_path("new_outlined_ttf_biohazard.png"))
         .unwrap();
     compare_image!(new_image, include_bytes!("reference_ttf_biohazard.png"));
 }
@@ -102,9 +98,7 @@ fn reference_outline_draw_ttf_biohazard() {
 #[test]
 fn reference_draw_otf_tailed_e() {
     let new_image = draw_grey_image(dev::rasterize_otf_tailed_e());
-    new_image
-        .save(target_dir().with_file_name("new_otf_tailed_e.png"))
-        .unwrap();
+    new_image.save(temp_path("new_otf_tailed_e.png")).unwrap();
     compare_image!(new_image, include_bytes!("reference_otf_tailed_e.png"));
 }
 
@@ -113,7 +107,7 @@ fn reference_outline_draw_otf_tailed_e() {
     let font = FontRef::try_from_slice(EXO2_OTF).unwrap();
     let new_image = outline_draw(font, 'ę', 300.0);
     new_image
-        .save(target_dir().with_file_name("new_outlined_otf_tailed_e.png"))
+        .save(temp_path("new_outlined_otf_tailed_e.png"))
         .unwrap();
     compare_image!(new_image, include_bytes!("reference_otf_tailed_e.png"));
 }
@@ -121,9 +115,7 @@ fn reference_outline_draw_otf_tailed_e() {
 #[test]
 fn reference_draw_ttf_tailed_e() {
     let new_image = draw_grey_image(dev::rasterize_ttf_tailed_e());
-    new_image
-        .save(target_dir().with_file_name("new_ttf_tailed_e.png"))
-        .unwrap();
+    new_image.save(temp_path("new_ttf_tailed_e.png")).unwrap();
     compare_image!(new_image, include_bytes!("reference_ttf_tailed_e.png"));
 }
 
@@ -132,9 +124,23 @@ fn reference_outline_draw_ttf_tailed_e() {
     let font = FontRef::try_from_slice(EXO2_TTF).unwrap();
     let new_image = outline_draw(font, 'ę', 300.0);
     new_image
-        .save(target_dir().with_file_name("new_outlined_ttf_tailed_e.png"))
+        .save(temp_path("new_outlined_ttf_tailed_e.png"))
         .unwrap();
     compare_image!(new_image, include_bytes!("reference_ttf_tailed_e.png"));
+}
+
+/// Cantarell f required an implicit outline "close" at the end.
+#[test]
+fn reference_outline_draw_cantarell_f() {
+    let font = FontRef::try_from_slice(CANTARELL_VF).unwrap();
+    let new_image = outline_draw(font, 'f', 300.0);
+    new_image
+        .save(temp_path("new_outlined_cantarell_f.png"))
+        .unwrap();
+    compare_image!(
+        new_image,
+        include_bytes!("reference_outlined_cantarell_f.png")
+    );
 }
 
 fn outline_draw<F: Font>(font: F, c: char, scale: f32) -> image::GrayAlphaImage {
--- glyph/src/ttfp/outliner.rs
+++ glyph/src/ttfp/outliner.rs
@@ -11,7 +11,9 @@ pub(crate) struct OutlineCurveBuilder {
 
 impl OutlineCurveBuilder {
     #[inline]
-    pub(crate) fn take_outline(self) -> Vec<OutlineCurve> {
+    pub(crate) fn take_outline(mut self) -> Vec<OutlineCurve> {
+        // some font glyphs implicitly close, e.g. Cantarell-VF.otf
+        owned_ttf_parser::OutlineBuilder::close(&mut self);
         self.outline
     }
 }
@@ -19,12 +21,14 @@ impl OutlineCurveBuilder {
 impl owned_ttf_parser::OutlineBuilder for OutlineCurveBuilder {
     #[inline]
     fn move_to(&mut self, x: f32, y: f32) {
+        // eprintln!("M {x} {y}");
         self.last = point(x, y);
         self.last_move = Some(self.last);
     }
 
     #[inline]
     fn line_to(&mut self, x1: f32, y1: f32) {
+        // eprintln!("L {x1} {y1}");
         let p1 = point(x1, y1);
         self.outline.push(OutlineCurve::Line(self.last, p1));
         self.last = p1;
@@ -32,6 +36,7 @@ impl owned_ttf_parser::OutlineBuilder for OutlineCurveBuilder {
 
     #[inline]
     fn quad_to(&mut self, x1: f32, y1: f32, x2: f32, y2: f32) {
+        // eprintln!("Q {x1} {y1}");
         let p1 = point(x1, y1);
         let p2 = point(x2, y2);
         self.outline.push(OutlineCurve::Quad(self.last, p1, p2));
@@ -40,6 +45,7 @@ impl owned_ttf_parser::OutlineBuilder for OutlineCurveBuilder {
 
     #[inline]
     fn curve_to(&mut self, x1: f32, y1: f32, x2: f32, y2: f32, x3: f32, y3: f32) {
+        // eprintln!("C {x1} {y1} {x3} {y3}");
         let p1 = point(x1, y1);
         let p2 = point(x2, y2);
         let p3 = point(x3, y3);
@@ -51,7 +57,8 @@ impl owned_ttf_parser::OutlineBuilder for OutlineCurveBuilder {
 
     #[inline]
     fn close(&mut self) {
-        if let Some(m) = self.last_move {
+        // eprintln!("Z");
+        if let Some(m) = self.last_move.take() {
             self.outline.push(OutlineCurve::Line(self.last, m));
         }
     }
